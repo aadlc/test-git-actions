ac8c19bc6bb3fdcd3914aa40e29b676436db64a8
Alfonzo Angulo <aangulo@cacaostudio.ca>
2021-01-29 12:19:35 -0700

    Duplicated code.js file outside src folder to test FILTER_REGEX_INCLUDE env variable

---
+++ code.js
@@ -0,0 +1,10 @@
+// duplicated this file to test FILTER_REGEX_INCLUDE env variable
+function test(name) {
+  console.log(name);
+}
+const firstName = 'pedro';
+
+const things = ['abc', 'abc', 'abc'];
+things.pop();
+
+test(firstName);
